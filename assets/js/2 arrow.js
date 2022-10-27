2fba4804d6edad2932356f2975d3ced25999eb20
Julio Lee <todocomerciolara@gmail.com>
2022-10-26 21:15:17 -0300

    first commit

---
+++ assets/js/2 arrow.js
@@ -0,0 +1,12 @@
+
+// Transforma las siguiente función a una arrow function de una línea. Este ejercicio
+// debe quedar en un archivo llamado `2 arrow.js`
+
+suma = function(a, b){
+    return a + b
+    }
+    
+
+// Repuesta
+
+suma = (a, b) => a + b;
---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+ele = document.getElementById("ele1")
+
+function pintar(color = 'green') {
+    ele.style.backgroundColor = color
+}
+
+ele.addEventListener("click", function () {
+    pintar('yellow')
+});
+
+// ---ultimo punto del desafio--- //
+
+
+select = document.getElementById("select")
+click1 = document.getElementById("click1")
+click2 = document.getElementById("click2")
+click3 = document.getElementById("click3")
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a') {
+        select.innerHTML = "A"
+        select.style.backgroundColor = "rgb(174, 137, 243)"
+        color = "rgb(174, 137, 243)"
+    }else if (event.key === 's') {
+        select.innerHTML = "S"
+        select.style.backgroundColor = "rgb(214, 149, 95)"
+        color = "rgb(214, 149, 95)"
+    }
+    else if (event.key === 'd') {
+        select.innerHTML = "D"
+        select.style.backgroundColor = "rgb(84, 177, 107)"
+        color = "rgb(84, 177, 107)"
+    }
+})
+
+click1.addEventListener("click", () => click1.style.backgroundColor= color);
+click2.addEventListener("click", () => click2.style.backgroundColor= color);
+click3.addEventListener("click", () => click3.style.backgroundColor= color);
